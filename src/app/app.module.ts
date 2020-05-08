d25872711e210c9b4855a421d26f80fa415c51b1
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-05-08 19:42:06 +0300

    nest quotes component in the app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalDetailsComponent,
+    QuoteFormComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
