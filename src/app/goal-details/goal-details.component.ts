2a6333806bb7d88365f63416cee31678b106b739
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-05-09 07:52:09 +0300

    add the like and unlike logic in the quote details component

--- src/app/goal-details/goal-details.component.ts
+++ src/app/goal-details/goal-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { Quotes } from '../quotes'
+
 
 @Component({
   selector: 'app-goal-details',
@@ -9,8 +10,15 @@ import { Quotes } from '../quotes'
 export class GoalDetailsComponent implements OnInit {
 
   @Input() quotes:Quotes;
+  @Output() complete= new EventEmitter<boolean>();
 
-  
+  like(quotes){
+    quotes.likes +=1
+  }
+
+  unlike(quotes){
+    quotes.unlikes +=1
+  }
 
 
   constructor() { }
